59beff27d9262b2ceb3e1a5316415ad5a64631f1
aco228 <35331284+aco228@users.noreply.github.com>
2020-06-02 22:02:16 +0200

    Initial commit

---
+++ 05_RTF_Reader/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _05_RTF_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblRow = new System.Windows.Forms.CheckBox();
+            this.lblDisplay = new System.Windows.Forms.RichTextBox();
+            this.btnS = new System.Windows.Forms.Button();
+            this.txtS = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(372, 25);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Otvori";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(390, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(354, 25);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Sacuvaj";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblRow
+            // 
+            this.lblRow.AutoSize = true;
+            this.lblRow.Location = new System.Drawing.Point(14, 32);
+            this.lblRow.Name = "lblRow";
+            this.lblRow.Size = new System.Drawing.Size(77, 18);
+            this.lblRow.TabIndex = 3;
+            this.lblRow.Text = "Jedan Red";
+            this.lblRow.UseVisualStyleBackColor = true;
+            this.lblRow.CheckedChanged += new System.EventHandler(this.lblRow_CheckedChanged);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDisplay.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(14, 57);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(730, 387);
+            this.lblDisplay.TabIndex = 4;
+            this.lblDisplay.Text = "";
+            // 
+            // btnS
+            // 
+            this.btnS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnS.Location = new System.Drawing.Point(686, 31);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(57, 22);
+            this.btnS.TabIndex = 5;
+            this.btnS.Text = "Trazi";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // txtS
+            // 
+            this.txtS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtS.Location = new System.Drawing.Point(527, 33);
+            this.txtS.Name = "txtS";
+            this.txtS.Size = new System.Drawing.Size(153, 20);
+            this.txtS.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 458);
+            this.Controls.Add(this.txtS);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lblRow);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(772, 496);
+            this.Name = "Form1";
+            this.Text = "Rtf Reader (aco228)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox lblRow;
+        private System.Windows.Forms.RichTextBox lblDisplay;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.TextBox txtS;
+    }
+}
+
---
+++ 05_RTF_Reader/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _05_RTF_Reader
+{
+    public partial class Form1 : Form
+    {
+        // Var
+        bool ucitano = false;
+        string FileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "RTF fajlovi (*.rtf)|*rtf| TXT fajlovi(*.txt)|*.txt";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                lblDisplay.Text = "Ucitavanje...";
+                FileName = op.FileName;
+                string text = File.ReadAllText(FileName);
+                string aba = obrada(text);
+                lblDisplay.Text = aba;
+                ucitano = true;
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ucitano)
+            {
+                SaveFileDialog sf = new SaveFileDialog();
+                sf.Filter = "RTF fajlovi (*.rtf)|*rtf TXT fajlovi(*.txt)|*.txt";
+                sf.Title = "Sacuvaj";
+                sf.FileName = FileName;
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    TextWriter tw = File.CreateText(sf.FileName);
+                    tw.Write(lblDisplay.Text); tw.Close();
+                }
+            }
+            else MessageBox.Show("Nista nije ucitano!");
+        }
+
+        //
+        // OBRADA TEKSTA
+        //
+        private string obrada(string a)
+        {
+            string back = "{";
+            a = a.Replace("\r", String.Empty); a = a.Replace("\n", String.Empty); a = a.Replace("\t", String.Empty);
+            int br = -1, mulLine = -1; bool izasao = false;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == '{') 
+                {
+                    br++; mulLine++;
+                    if(br == 0) back += "\r\n\r\n" + getSpace(br);
+                    else back += "\r\n" + getSpace(br);
+                }
+                if (a[i] == '}') 
+                { 
+                    br--;
+                    if (mulLine == 0 && br == -1) { back += "}"; mulLine = -1; }
+                    if (br == -1 && mulLine > 0) { back += "\r\n}"; mulLine = -1;  }
+                    if (br >= 0) { back += "}\r\n" + getSpace(br); }
+
+                    izasao = true;
+                    continue;
+                }
+                if (a[i] == '\\' && izasao && br == -1) { back += "\r\n\r\n"; izasao = false; }
+                back += a[i];
+            }
+            return back;
+        }
+        private string getSpace(int a)
+        {
+            string back= "", space = "    ";
+            for (int i = 0; i < a; i++) back += space;
+            return back;
+        }
+        private string getFileName(string a)
+        {
+            string back = "";
+            int len = a.Length;
+            for (int i = a.Length-1; i >= 0; i--)
+            {
+                //if (a[i] == '\\') MessageBox.Show(a.Substring(i, a.Length - 2)); 
+            }
+            //MessageBox.Show(back);
+            return back;
+        }
+
+        private void lblRow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lblRow.Checked) { lblDisplay.WordWrap = true; }
+            if (!lblRow.Checked) lblDisplay.WordWrap = false;
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+            if (txtS.Text != "")
+            {
+                search(txtS.Text);
+            }
+        }
+        private void search(string a)
+        {
+            int curPos = lblDisplay.SelectionStart;
+            int selectLen = lblDisplay.SelectionLength;
+            int len = a.Length;
+            int i = lblDisplay.Text.IndexOf(a);
+
+            string temp = lblDisplay.Text; lblDisplay.Text = ""; lblDisplay.Text = temp;
+
+            while (i >= 0)
+            {
+                lblDisplay.Select(i, len);
+                //lblDisplay.SelectionFont = new Font(me.SelectionFont, style);
+                lblDisplay.SelectionBackColor = Color.FromArgb(196, 255, 252);
+                i = lblDisplay.Text.IndexOf(a, i + len);
+            }
+
+            lblDisplay.SelectionStart = curPos;
+            lblDisplay.SelectionLength = selectLen;
+        }
+
+
+    }
+}
